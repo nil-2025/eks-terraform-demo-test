a2e72f5d969748bea9ace37dcef72b80bc578df1
nil-2025 <nilotpal.bhuniya@gmail.com>
2026-03-16 18:11:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,8 @@ module "eks" {
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
+  enable_cluster_creator_admin_permissions = true
+
   eks_managed_node_groups = {
     default = {
       instance_types = ["t3.medium"]
